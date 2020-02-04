ab79d385260eb3ad4816bdb9a8d3cc0ec415be3d
haybnann <haybnann@ut.utm.edu>
2020-02-04 13:15:42 -0600

    Update main.c

--- shell/main.c
+++ shell/main.c
@@ -147,12 +147,6 @@ void shellMode(){
 
 	}
 }
-void printDir() 
-{ 
-    char cwd[1024]; 
-    getcwd(cwd, sizeof(cwd)); 
-    printf("\nDir: %s", cwd); 
-} 
 
 void displayPrompt(){
 	printf("prompt> ");
